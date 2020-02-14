0d2aae2c1e819b0d498507bde15cd6488ba768d4
Gabriel <gabsanchez438@gmail.com>
2020-02-14 09:34:58 -0600

    Fixed update and delete

--- repositories/users.js
+++ repositories/users.js
@@ -1,54 +1,52 @@
 const User = require('../models').User
 
-const getUsers = (request, response) => {
+async function getUsers(request, response){
     return User.findAll({attributes : ['id', 'name', 'email']})
 }
 
-const getUserById = (request, response) => {
+async function getUserById(request, response){
     return User.findOne({
         where: { id: Number(request.params.id) },
         attributes : ['id', 'name', 'email']
     })
 }
 
-const createUser = (request, response) => {
+async function createUser(request, response){
     return User.create(request.body)
 }
 
-const updateUser = (request, response) => {
-    return User.findOne({
+async function updateUser(request, response){
+    var userToUpdate = await User.findOne({
         where: { 
             id: Number(request.params.id) 
         },
         attributes : ['id', 'name', 'email']
     })
-    .then((userToUpdate) => {
-        if(userToUpdate){
-            return User.update({
-                where: {
-                    id: Number(request.params.id)
-                }
-            })
-        }
-    })
+    if(userToUpdate){
+        return User.update(request.body,{
+            where: {
+                id: Number(userToUpdate.id)
+            }
+        })
+    }
+    throw "User not found"
 }
 
-const deleteUser = (request, response) => {
-    return User.findOne({
+async function deleteUser(request, response) {
+    var userToDelete = await User.findOne({
         where: { 
             id: Number(request.params.id) 
         },
         attributes : ['id', 'name', 'email']
     })
-    .then((userToDelete) => {
-        if(userToDelete){
-            return User.destroy({
-                where: {
-                    id: Number(request.params.id)
-                }
-            })
-        }
-    })  
+    if(userToDelete){
+        return User.destroy({
+            where: {
+                id: Number(request.params.id)
+            }
+        })
+    }
+    throw "User not found"
 }
 
 module.exports = {
